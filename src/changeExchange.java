b2e7858c49eae3e2f007db0fee7e2e1a5e6fcc99
Kevin Garcia <kevingarciaestala@outlook.com>
2024-01-05 14:18:57 -0600

    Created Interface and classes

---
+++ src/changeExchange.java
@@ -0,0 +1,21 @@
+public class changeExchange implements Interface {
+
+    @Override
+    public void subtractingMax(double Change) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'subtractingMax'");
+    }
+
+    @Override
+    public void splitAndCount(double Change) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'splitAndCount'");
+    }
+
+    @Override
+    public void userInput() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'userInput'");
+    }
+
+}
